1907a0da77b6ab709111cfcb16fe427375d7e8b8
ShlomoSerber <ss.negf@gmail.com>
2022-02-27 13:19:46 -0300

    started

--- 6 - TaTeTi/tateti/src/components/SetPlayers/SetPlayers.js
+++ 6 - TaTeTi/tateti/src/components/SetPlayers/SetPlayers.js
@@ -3,7 +3,11 @@ import "./SetPlayers.css"
 const SetPlayers = () => {
     return (
         <div className="main-container">
-            <h1 className="main-title">Set player names</h1>
+            <div className="game-title">
+                <img src={require("../assets/images/tateti.jpg")} alt="" className="game-logo"></img>
+                <h1 className="game-name">Ta Te Ti</h1>
+            </div>
+            <h2 className="main-title">Set player names</h2>
             <form className="players-form">
                 <div className="players">
                     <div className="player">
