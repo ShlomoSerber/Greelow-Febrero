85bb47218b79d530a42263e18fddfcdb0aa19524
ShlomoSerber <ss.negf@gmail.com>
2022-03-17 19:34:40 -0300

    changed tateti to single screen

--- 6 - TaTeTi/tateti/src/App.js
+++ 6 - TaTeTi/tateti/src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import SetPlayers from "./components/SetPlayers"
-import Game from "./components/Game"
+import Tateti from './components/Tateti/Tateti';
 
 function App() {
   return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<SetPlayers />}></Route>
-            <Route path="/game" element={<Game />}></Route>
-        </Routes>
-    </Router>
+    <Tateti />
   );
 }
 
--- 6 - TaTeTi/tateti/src/components/Game/Game.js
+++
@@ -1,9 +0,0 @@
-import "./Game.css"
-
-const Game = () => {
-    return ( 
-        <h1>Game</h1>
-    );
-}
-
-export default Game;
