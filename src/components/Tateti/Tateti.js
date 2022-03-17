b90c650bb441b4b54f08bfeb40d8bcdb5caa5b10
ShlomoSerber <ss.negf@gmail.com>
2022-03-17 20:46:31 -0300

    added outline code

--- src/components/Tateti/Tateti.js
+++ src/components/Tateti/Tateti.js
@@ -1,11 +1,45 @@
+import { useState, useEffect } from "react";
 import "./Tateti.css"
 
 const Tateti = () => {
-    return ( 
+    const [values, setValues] = useState([]);
+    const [playing, setPlaying] = useState(false);
+    const [player, setPlayer] = useState("X");
+    const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    useEffect(() => {
+        
+    }, [values])
+
+    const squareClick = (event, squareNumber) => {
+        event.target.value = player;
+
+        setValues([{
+            number: squareNumber,
+            value: player
+        }, ...values]);
+
+        if (player == "X") {
+            setPlayer("O")
+        } else if (player == "O") {
+            setPlayer("X")
+        }
+
+    }
+
+    return (
         <div className="main-container">
             <div className="game-title">
                 <h1 className="game-name">Ta Te Ti</h1>
                 <img className="game-logo" src={require("../../assets/images/tateti.jpg")}></img>
+            </div>
+            <h2 className="current-player">{`It's ${player} Turn!`}</h2>
+            <div className="game-squares">
+                {
+                squares.map(square => 
+                    <div className="game-square" key={square} onClick={() => squareClick(event, square)}></div>
+                )
+                }
             </div>
         </div>
     );
